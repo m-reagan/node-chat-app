ddd59792e6dcefba86ef297b33f0da5bfbfd7536
Reagan <leoreagan@gmail.com>
2017-11-28 19:05:58 -0500

    Chat app initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const express = require ('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.listen(port,  () => {
+  console.log(`Server started successfully at port ${port}`);
+});
